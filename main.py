305725e65d063e1b079154d557006632afd791aa
Marcus Kempe <makem86@gmail.com>
2015-02-10 10:49:40 +0100

    Added license

--- main.py
+++ main.py
@@ -1,3 +1,41 @@
+# -*- coding: utf-8 -*-
+# Author: Marcus Kempe
+#
+# Example data file can be downloaded from: http://www.lantmateriet.se/globalassets/kartor-och-geografisk-information/download/demo_grid2_5x5.zip
+# Look for the .asc-file.
+#
+# Parts of this file is based on the following vispy gallery example: 
+# 
+# http://vispy.org/examples/demo/gloo/terrain.html
+# 
+# Any material contained in this document that can be directly backtracked to this example is
+# licensed under that documents own license, everything else is unlicensed as follows: 
+#
+# This is free and unencumbered software released into the public domain.
+
+# Anyone is free to copy, modify, publish, use, compile, sell, or
+# distribute this software, either in source code form or as a compiled
+# binary, for any purpose, commercial or non-commercial, and by any
+# means.
+
+# In jurisdictions that recognize copyright laws, the author or authors
+# of this software dedicate any and all copyright interest in the
+# software to the public domain. We make this dedication for the benefit
+# of the public at large and to the detriment of our heirs and
+# successors. We intend this dedication to be an overt act of
+# relinquishment in perpetuity of all present and future rights to this
+# software under copyright law.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+
+# For more information, please refer to <http://unlicense.org/>
+
 import numpy as np
 import math
 from vispy import app
